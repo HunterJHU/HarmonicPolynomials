88cfd84ff11244784ce72fbbc835a92819383451
Hunter <bjackson4@mail.usf.edu>
2014-09-11 21:48:30 -0400

    complex contour plotting

---
+++ complexcontour.py
@@ -0,0 +1,21 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+# generate data
+x = np.r_[0:10:30j]
+y = np.r_[0:10:20j]
+X, Y = np.meshgrid(x, y)
+Z = X*np.exp(2j*Y) # some arbitrary complex data
+
+# plot it
+def plotit(z, title):
+    cs = plt.contour(X,Y,z) # contour() accepts complex values
+    plt.clabel(cs, inline=1, fontsize=10) # add labels to contours
+    plt.title(title)
+    plt.savefig(title+'.png')
+
+plotit(Z, 'real')
+plotit(Z.real, 'explicit real')
+plotit(Z.imag, 'imaginary')
+
+plt.show()
